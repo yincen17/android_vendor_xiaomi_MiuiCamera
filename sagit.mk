e89624dcc25d6784c6270c3d81abab87a09d8831
soshiweekly <1376106928@qq.com>
2018-10-14 00:20:39 +0800

    xiaomi: upload prebuild MiuiCamera

---
+++ sagit.mk
@@ -0,0 +1,3 @@
+PRODUCT_COPY_FILES += \
+    $(VENDOR_PATH)/system/etc/device_features/sagit.xml:system/etc/device_features/sagit.xml \
+    $(VENDOR_PATH)/vendor/etc/dualcamera_sagit.png:$(TARGET_COPY_OUT_VENDOR)/etc/dualcamera.png
---
+++ tiffany.mk
@@ -0,0 +1,3 @@
+PRODUCT_COPY_FILES += \
+    $(VENDOR_PATH)/system/etc/device_features/tiffany.xml:system/etc/device_features/tiffany.xml \
+    $(VENDOR_PATH)/vendor/etc/dualcamera_tiffany.png:$(TARGET_COPY_OUT_VENDOR)/etc/dualcamera.png
