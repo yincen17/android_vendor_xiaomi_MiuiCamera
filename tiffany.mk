3bc3d1f36ef15ad8677d492e0fb4246f7313825c
BLIBWU <mailto.magicalunicorn@gmail.com>
2020-04-30 00:14:18 +0200

    tiffany: Remove watermark & Update device_features.xml

--- tiffany.mk
+++ tiffany.mk
@@ -1,3 +1,2 @@
 PRODUCT_COPY_FILES += \
-    $(VENDOR_PATH)/system/etc/device_features/tiffany.xml:system/etc/device_features/tiffany.xml \
-    $(VENDOR_PATH)/vendor/etc/dualcamera_tiffany.png:$(TARGET_COPY_OUT_VENDOR)/etc/dualcamera.png
+    $(VENDOR_PATH)/system/etc/device_features/tiffany.xml:system/etc/device_features/tiffany.xml
