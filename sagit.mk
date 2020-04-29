555e8e711a6af539bf17e1e65016ef2fc086a958
BLIBWU <mailto.magicalunicorn@gmail.com>
2020-04-29 15:43:50 +0200

    sagit: Remove Sagit support

--- sagit.mk
+++
@@ -1,3 +0,0 @@
-PRODUCT_COPY_FILES += \
-    $(VENDOR_PATH)/system/etc/device_features/sagit.xml:system/etc/device_features/sagit.xml \
-    $(VENDOR_PATH)/vendor/etc/dualcamera_sagit.png:$(TARGET_COPY_OUT_VENDOR)/etc/dualcamera.png
